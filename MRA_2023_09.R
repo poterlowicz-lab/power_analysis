46600bfc87327289060eb0ae18308093d7b3932c
Krzysztof Poterlowicz <k.poterlowicz1@bradford.ac.uk>
2023-09-08 16:51:42 +0100

    s

---
+++ MRA_2023_09.R
@@ -0,0 +1,11 @@
+library(pwr)
+pwr.p.test(h = ES.h(p1 = 0.03, p2 = 0.01),
+           sig.level = 0.01,
+           power = 0.8)
+pwr.2p2n.test(h = ES.h(p1 = 0.03, p2 = 0.01), n1 = 500, sig.level = 0.05, power= 0.8)
+
+
+prob <- matrix(c(0.03,0.01,0.01,0.01), ncol=2,
+               dimnames = list(c("RA","No"),c("Mult-CH","No-CH")))
+prob
+ES.w2(prob)
