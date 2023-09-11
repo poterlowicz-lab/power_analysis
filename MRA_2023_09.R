88f72b8f6a112d463df86e566546442161fed9b1
Krzysztof Poterlowicz <k.poterlowicz1@bradford.ac.uk>
2023-09-11 13:17:50 +0100

    update to include Trio

--- MRA_2023_09.R
+++ MRA_2023_09.R
@@ -9,3 +9,15 @@ prob <- matrix(c(0.03,0.01,0.01,0.01), ncol=2,
                dimnames = list(c("RA","No"),c("Mult-CH","No-CH")))
 prob
 ES.w2(prob)
+
+
+######################
+#if (!require("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#
+#BiocManager::install("trio")
+###
+library(trio)
+trio.power(maf = 0.1, RR = 3, alpha = 5*10^(-8), n = NULL, beta = 0.8,
+           model = c("additive", "dominant", "recessive"), test = c("gTDT", "Score", "aTDT"))
+tr
